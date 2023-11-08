f00a46f4c6b5d9a0168c2a3f1fd26c07676bd076
Luis A. Medina <lmedinar1503@alumno.ipn.mx>
2023-11-08 12:10:07 -0600

    Agregado un campo para adjuntar a una bitácora digital.

--- vortex2.py
+++ vortex2.py
@@ -14,14 +14,10 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(748, 628)
+        MainWindow.resize(1203, 1055)
         MainWindow.setMaximumSize(QtCore.QSize(16018730, 16777215))
         icon = QtGui.QIcon()
-        icon.addPixmap(
-            QtGui.QPixmap(":/Iconos/assets/icon.ico"),
-            QtGui.QIcon.Normal,
-            QtGui.QIcon.Off,
-        )
+        icon.addPixmap(QtGui.QPixmap(":/Iconos/assets/icon.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         MainWindow.setWindowIcon(icon)
         self.layoutCenterAll = QtWidgets.QWidget(MainWindow)
         self.layoutCenterAll.setObjectName("layoutCenterAll")
@@ -30,10 +26,9 @@ class Ui_MainWindow(object):
         self.layoutMainVertical = QtWidgets.QVBoxLayout()
         self.layoutMainVertical.setObjectName("layoutMainVertical")
         self.labelSeccion1 = QtWidgets.QLabel(self.layoutCenterAll)
-        self.labelSeccion1.setStyleSheet("font-weight: 900;\n" "font-size: 15pt;")
-        self.labelSeccion1.setAlignment(
-            QtCore.Qt.AlignLeading | QtCore.Qt.AlignLeft | QtCore.Qt.AlignTop
-        )
+        self.labelSeccion1.setStyleSheet("font-weight: 900;\n"
+"font-size: 15pt;")
+        self.labelSeccion1.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
         self.labelSeccion1.setObjectName("labelSeccion1")
         self.layoutMainVertical.addWidget(self.labelSeccion1)
         self.layoutArduinoSection = QtWidgets.QHBoxLayout()
@@ -48,14 +43,10 @@ class Ui_MainWindow(object):
         self.comboBox.setMinimumSize(QtCore.QSize(100, 0))
         self.comboBox.setObjectName("comboBox")
         self.layoutArduinoSection.addWidget(self.comboBox)
-        spacerItem = QtWidgets.QSpacerItem(
-            40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum
-        )
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.layoutArduinoSection.addItem(spacerItem)
         self.layoutMainVertical.addLayout(self.layoutArduinoSection)
-        spacerItem1 = QtWidgets.QSpacerItem(
-            20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding
-        )
+        spacerItem1 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         self.layoutMainVertical.addItem(spacerItem1)
         self.lineLong1 = QtWidgets.QFrame(self.layoutCenterAll)
         self.lineLong1.setSizeIncrement(QtCore.QSize(0, 0))
@@ -66,34 +57,25 @@ class Ui_MainWindow(object):
         self.lineLong1.setObjectName("lineLong1")
         self.layoutMainVertical.addWidget(self.lineLong1)
         self.labelSeccion2 = QtWidgets.QLabel(self.layoutCenterAll)
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed
-        )
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            self.labelSeccion2.sizePolicy().hasHeightForWidth()
-        )
+        sizePolicy.setHeightForWidth(self.labelSeccion2.sizePolicy().hasHeightForWidth())
         self.labelSeccion2.setSizePolicy(sizePolicy)
-        self.labelSeccion2.setStyleSheet("font-weight: 900;\n" "font-size: 15pt;")
+        self.labelSeccion2.setStyleSheet("font-weight: 900;\n"
+"font-size: 15pt;")
         self.labelSeccion2.setObjectName("labelSeccion2")
         self.layoutMainVertical.addWidget(self.labelSeccion2)
         self.layoutMainVertical2 = QtWidgets.QVBoxLayout()
         self.layoutMainVertical2.setObjectName("layoutMainVertical2")
         self.layoutImageCenter = QtWidgets.QHBoxLayout()
         self.layoutImageCenter.setObjectName("layoutImageCenter")
-        spacerItem2 = QtWidgets.QSpacerItem(
-            10, 10, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum
-        )
+        spacerItem2 = QtWidgets.QSpacerItem(10, 10, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
         self.layoutImageCenter.addItem(spacerItem2)
-        spacerItem3 = QtWidgets.QSpacerItem(
-            20, 150, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Maximum
-        )
+        spacerItem3 = QtWidgets.QSpacerItem(20, 150, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Maximum)
         self.layoutImageCenter.addItem(spacerItem3)
         self.labelSignal = QtWidgets.QLabel(self.layoutCenterAll)
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Ignored, QtWidgets.QSizePolicy.Ignored
-        )
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Ignored, QtWidgets.QSizePolicy.Ignored)
         sizePolicy.setHorizontalStretch(1)
         sizePolicy.setVerticalStretch(1)
         sizePolicy.setHeightForWidth(self.labelSignal.sizePolicy().hasHeightForWidth())
@@ -109,9 +91,7 @@ class Ui_MainWindow(object):
         self.labelSignal.setOpenExternalLinks(False)
         self.labelSignal.setObjectName("labelSignal")
         self.layoutImageCenter.addWidget(self.labelSignal)
-        spacerItem4 = QtWidgets.QSpacerItem(
-            20, 10, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum
-        )
+        spacerItem4 = QtWidgets.QSpacerItem(20, 10, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
         self.layoutImageCenter.addItem(spacerItem4)
         self.layoutMainVertical2.addLayout(self.layoutImageCenter)
         self.layoutMainVertical.addLayout(self.layoutMainVertical2)
@@ -151,19 +131,13 @@ class Ui_MainWindow(object):
         self.inRadioSegs.setObjectName("inRadioSegs")
         self.layoutParams.addWidget(self.inRadioSegs, 6, 0, 1, 1)
         self.labelTiempoEstable = QtWidgets.QLabel(self.layoutCenterAll)
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed
-        )
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            self.labelTiempoEstable.sizePolicy().hasHeightForWidth()
-        )
+        sizePolicy.setHeightForWidth(self.labelTiempoEstable.sizePolicy().hasHeightForWidth())
         self.labelTiempoEstable.setSizePolicy(sizePolicy)
         self.labelTiempoEstable.setStyleSheet("font-weight:900;")
-        self.labelTiempoEstable.setAlignment(
-            QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter
-        )
+        self.labelTiempoEstable.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.labelTiempoEstable.setObjectName("labelTiempoEstable")
         self.layoutParams.addWidget(self.labelTiempoEstable, 5, 0, 1, 1)
         self.miniline1 = QtWidgets.QFrame(self.layoutCenterAll)
@@ -203,25 +177,19 @@ class Ui_MainWindow(object):
         self.layoutParams.addWidget(self.miniline2, 2, 0, 1, 1)
         self.labelVelocidadMax = QtWidgets.QLabel(self.layoutCenterAll)
         self.labelVelocidadMax.setStyleSheet("font-weight:900;")
-        self.labelVelocidadMax.setAlignment(
-            QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter
-        )
+        self.labelVelocidadMax.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.labelVelocidadMax.setObjectName("labelVelocidadMax")
         self.layoutParams.addWidget(self.labelVelocidadMax, 3, 0, 1, 1)
         self.inRadioMins = QtWidgets.QRadioButton(self.layoutCenterAll)
         self.inRadioMins.setAcceptDrops(False)
         self.inRadioMins.setLayoutDirection(QtCore.Qt.RightToLeft)
         self.inRadioMins.setAutoFillBackground(False)
-        self.inRadioMins.setLocale(
-            QtCore.QLocale(QtCore.QLocale.Spanish, QtCore.QLocale.Mexico)
-        )
+        self.inRadioMins.setLocale(QtCore.QLocale(QtCore.QLocale.Spanish, QtCore.QLocale.Mexico))
         self.inRadioMins.setObjectName("inRadioMins")
         self.layoutParams.addWidget(self.inRadioMins, 7, 0, 1, 1)
         self.labelTiempoEstimadoTxt = QtWidgets.QLabel(self.layoutCenterAll)
         self.labelTiempoEstimadoTxt.setStyleSheet("font-weight:900;")
-        self.labelTiempoEstimadoTxt.setAlignment(
-            QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter
-        )
+        self.labelTiempoEstimadoTxt.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.labelTiempoEstimadoTxt.setObjectName("labelTiempoEstimadoTxt")
         self.layoutParams.addWidget(self.labelTiempoEstimadoTxt, 11, 0, 1, 1)
         self.inMinutos = QtWidgets.QSpinBox(self.layoutCenterAll)
@@ -234,9 +202,7 @@ class Ui_MainWindow(object):
         self.layoutParams.addWidget(self.inMinutos, 7, 1, 1, 1)
         self.layoutTiempoSalida = QtWidgets.QLabel(self.layoutCenterAll)
         self.layoutTiempoSalida.setStyleSheet("font-weight: 900;")
-        self.layoutTiempoSalida.setAlignment(
-            QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter
-        )
+        self.layoutTiempoSalida.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.layoutTiempoSalida.setObjectName("layoutTiempoSalida")
         self.layoutParams.addWidget(self.layoutTiempoSalida, 1, 0, 1, 1)
         self.labelTiempoEstimado = QtWidgets.QLabel(self.layoutCenterAll)
@@ -244,9 +210,7 @@ class Ui_MainWindow(object):
         self.layoutParams.addWidget(self.labelTiempoEstimado, 11, 1, 1, 1)
         self.labelTiempoFrenado = QtWidgets.QLabel(self.layoutCenterAll)
         self.labelTiempoFrenado.setStyleSheet("font-weight:900;")
-        self.labelTiempoFrenado.setAlignment(
-            QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter
-        )
+        self.labelTiempoFrenado.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.labelTiempoFrenado.setObjectName("labelTiempoFrenado")
         self.layoutParams.addWidget(self.labelTiempoFrenado, 9, 0, 1, 1)
         self.line = QtWidgets.QFrame(self.layoutCenterAll)
@@ -256,14 +220,10 @@ class Ui_MainWindow(object):
         self.line.setObjectName("line")
         self.layoutParams.addWidget(self.line, 10, 0, 1, 1)
         self.layoutMainVertical.addLayout(self.layoutParams)
-        spacerItem5 = QtWidgets.QSpacerItem(
-            20, 100, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding
-        )
+        spacerItem5 = QtWidgets.QSpacerItem(20, 100, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         self.layoutMainVertical.addItem(spacerItem5)
         self.lineLong2 = QtWidgets.QFrame(self.layoutCenterAll)
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed
-        )
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.lineLong2.sizePolicy().hasHeightForWidth())
@@ -276,25 +236,20 @@ class Ui_MainWindow(object):
         self.lineLong2.setObjectName("lineLong2")
         self.layoutMainVertical.addWidget(self.lineLong2)
         self.labelSeccion3 = QtWidgets.QLabel(self.layoutCenterAll)
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed
-        )
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            self.labelSeccion3.sizePolicy().hasHeightForWidth()
-        )
+        sizePolicy.setHeightForWidth(self.labelSeccion3.sizePolicy().hasHeightForWidth())
         self.labelSeccion3.setSizePolicy(sizePolicy)
-        self.labelSeccion3.setStyleSheet("font-weight: 900;\n" "font-size: 15pt;")
+        self.labelSeccion3.setStyleSheet("font-weight: 900;\n"
+"font-size: 15pt;")
         self.labelSeccion3.setObjectName("labelSeccion3")
         self.layoutMainVertical.addWidget(self.labelSeccion3)
         self.centerControl1 = QtWidgets.QHBoxLayout()
         self.centerControl1.setObjectName("centerControl1")
         self.centerControl2 = QtWidgets.QHBoxLayout()
         self.centerControl2.setObjectName("centerControl2")
-        spacerItem6 = QtWidgets.QSpacerItem(
-            40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum
-        )
+        spacerItem6 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.centerControl2.addItem(spacerItem6)
         self.buttonIniciar = QtWidgets.QPushButton(self.layoutCenterAll)
         self.buttonIniciar.setObjectName("buttonIniciar")
@@ -303,9 +258,7 @@ class Ui_MainWindow(object):
         self.buttonDetener.setEnabled(False)
         self.buttonDetener.setObjectName("buttonDetener")
         self.centerControl2.addWidget(self.buttonDetener)
-        spacerItem7 = QtWidgets.QSpacerItem(
-            40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum
-        )
+        spacerItem7 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.centerControl2.addItem(spacerItem7)
         self.centerControl1.addLayout(self.centerControl2)
         self.layoutMainVertical.addLayout(self.centerControl1)
@@ -320,9 +273,7 @@ class Ui_MainWindow(object):
         self.progressBar2.setObjectName("progressBar2")
         self.layoutBarras2.addWidget(self.progressBar2, 1, 1, 1, 1)
         self.labelBarra1 = QtWidgets.QLabel(self.layoutCenterAll)
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed
-        )
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.labelBarra1.sizePolicy().hasHeightForWidth())
@@ -334,9 +285,7 @@ class Ui_MainWindow(object):
         self.progressBar1.setObjectName("progressBar1")
         self.layoutBarras2.addWidget(self.progressBar1, 1, 0, 1, 1)
         self.labelBarra2 = QtWidgets.QLabel(self.layoutCenterAll)
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed
-        )
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.labelBarra2.sizePolicy().hasHeightForWidth())
@@ -352,10 +301,37 @@ class Ui_MainWindow(object):
         self.layoutBarras2.addWidget(self.progressBar3, 1, 2, 1, 1)
         self.layoutBarras1.addLayout(self.layoutBarras2)
         self.layoutMainVertical.addLayout(self.layoutBarras1)
+        spacerItem8 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
+        self.layoutMainVertical.addItem(spacerItem8)
+        self.lineLong3 = QtWidgets.QFrame(self.layoutCenterAll)
+        self.lineLong3.setLineWidth(3)
+        self.lineLong3.setMidLineWidth(3)
+        self.lineLong3.setFrameShape(QtWidgets.QFrame.HLine)
+        self.lineLong3.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.lineLong3.setObjectName("lineLong3")
+        self.layoutMainVertical.addWidget(self.lineLong3)
+        self.labelSection4 = QtWidgets.QLabel(self.layoutCenterAll)
+        self.labelSection4.setStyleSheet("font-weight: 900;\n"
+"font-size: 15pt;")
+        self.labelSection4.setObjectName("labelSection4")
+        self.layoutMainVertical.addWidget(self.labelSection4)
+        self.textEdit = QtWidgets.QTextEdit(self.layoutCenterAll)
+        self.textEdit.setObjectName("textEdit")
+        self.layoutMainVertical.addWidget(self.textEdit)
+        self.layoutSubirBitacora = QtWidgets.QHBoxLayout()
+        self.layoutSubirBitacora.setObjectName("layoutSubirBitacora")
+        spacerItem9 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.layoutSubirBitacora.addItem(spacerItem9)
+        self.buttonBitacora = QtWidgets.QPushButton(self.layoutCenterAll)
+        self.buttonBitacora.setObjectName("buttonBitacora")
+        self.layoutSubirBitacora.addWidget(self.buttonBitacora)
+        spacerItem10 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.layoutSubirBitacora.addItem(spacerItem10)
+        self.layoutMainVertical.addLayout(self.layoutSubirBitacora)
         self.horizontalLayout.addLayout(self.layoutMainVertical)
         MainWindow.setCentralWidget(self.layoutCenterAll)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 748, 22))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1203, 32))
         self.menubar.setObjectName("menubar")
         self.menuA_cerca_de = QtWidgets.QMenu(self.menubar)
         self.menuA_cerca_de.setObjectName("menuA_cerca_de")
@@ -383,17 +359,11 @@ class Ui_MainWindow(object):
         self.labelUnidades1.setText(_translate("MainWindow", "segundos"))
         self.inRadioSegs.setText(_translate("MainWindow", "Segundos:"))
         self.labelTiempoEstable.setText(_translate("MainWindow", "Tiempo estable:"))
-        self.labelVelocidadMax.setText(
-            _translate("MainWindow", "Velocidad máxima (estable):")
-        )
+        self.labelVelocidadMax.setText(_translate("MainWindow", "Velocidad máxima (estable):"))
         self.inRadioMins.setText(_translate("MainWindow", "Minutos:"))
-        self.labelTiempoEstimadoTxt.setText(
-            _translate("MainWindow", "Tiempo total estimado:")
-        )
+        self.labelTiempoEstimadoTxt.setText(_translate("MainWindow", "Tiempo total estimado:"))
         self.layoutTiempoSalida.setText(_translate("MainWindow", "Tiempo de subida:"))
-        self.labelTiempoEstimado.setText(
-            _translate("MainWindow", "2 minutos con 40 segundos")
-        )
+        self.labelTiempoEstimado.setText(_translate("MainWindow", "2 minutos con 40 segundos"))
         self.labelTiempoFrenado.setText(_translate("MainWindow", "Tiempo de bajada:"))
         self.labelSeccion3.setText(_translate("MainWindow", "Iniciar/Detener:"))
         self.buttonIniciar.setText(_translate("MainWindow", "Iniciar"))
@@ -401,16 +371,15 @@ class Ui_MainWindow(object):
         self.labelBarra1.setText(_translate("MainWindow", "Aceleración"))
         self.labelBarra2.setText(_translate("MainWindow", "Velocidad constante"))
         self.labelBarra3.setText(_translate("MainWindow", "Frenado"))
+        self.labelSection4.setText(_translate("MainWindow", "Notas del experimento (bitácora digital):"))
+        self.buttonBitacora.setText(_translate("MainWindow", "Subir a bitácora"))
         self.menuA_cerca_de.setTitle(_translate("MainWindow", "Ayuda"))
         self.actionAcerca_de.setText(_translate("MainWindow", "Acerca de"))
-
-
 import resources_rc
 
 
 if __name__ == "__main__":
     import sys
-
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
